ada2ad613189944f7aa7df367713fc67dde2a079
vikky kumar <vikkykumargupta7667@gmail.com>
2026-09-06 00:40:46 +0530

    remove autowired and add requiredArgsConstructor annotation

--- inventory-service/src/main/java/com/resiliencelab/inventory/service/controller/InventoryController.java
+++ inventory-service/src/main/java/com/resiliencelab/inventory/service/controller/InventoryController.java
@@ -5,17 +5,19 @@ import com.resiliencelab.inventory.service.dto.InventoryResponse;
 import com.resiliencelab.inventory.service.dto.ReserveInventoryRequest;
 import com.resiliencelab.inventory.service.service.InventoryService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+
 @RestController
 @RequestMapping("/api/inventory")
+@RequiredArgsConstructor
 public class InventoryController {
-    @Autowired
-    private InventoryService inventoryService;
+
+    private final InventoryService inventoryService;
 
     @GetMapping("/{productId}")
     public ResponseEntity<InventoryResponse> getInventory(@PathVariable String productId){
@@ -29,6 +31,7 @@ public class InventoryController {
             @PathVariable String productId,
             @Valid @RequestBody ReserveInventoryRequest request
     ) throws InterruptedException {
+
 
         return ResponseEntity.ok(
                 inventoryService.reserveInventory(productId, request)
--- payment-service/src/main/java/com/resiliencelab/payment/service/controller/PaymentController.java
+++ payment-service/src/main/java/com/resiliencelab/payment/service/controller/PaymentController.java
@@ -4,21 +4,22 @@ import com.resiliencelab.payment.service.dto.PaymentRequest;
 import com.resiliencelab.payment.service.dto.PaymentResponse;
 import com.resiliencelab.payment.service.service.PaymentService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/payments")
+@RequiredArgsConstructor
 public class PaymentController {
 
-    @Autowired
-    private PaymentService paymentService;
+    private final PaymentService paymentService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED )
     public ResponseEntity<PaymentResponse> processPayment(@Valid @RequestBody PaymentRequest request){
+
            return ResponseEntity.ok(paymentService.processPayment(request));
     }
 }
